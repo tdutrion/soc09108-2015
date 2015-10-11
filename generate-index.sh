23af6e22149a4a712d8210423defb2cdead25f7b
Thomas Dutrion <thomas@engineor.com>
2015-10-11 22:47:13 +0100

    Add practicals + styles

--- generate-index.sh
+++ generate-index.sh
@@ -8,12 +8,28 @@ echo "  <title>SOC09108 - 2015</title>" >> $OUTPUT
 echo "  <link href='./style.css' rel='stylesheet' type='text/css'>" >> $OUTPUT
 echo "</head>" >> $OUTPUT
 echo "<body>" >> $OUTPUT
-echo "<h1>SOC09108 - 2015 - Lectures</h1>" >> $OUTPUT
-echo "  <ul>" >> $OUTPUT
+echo "  <img id=\"logo\" src=\"./ENU-logo.png\" alt=\"Edinburgh Napier University logo\">" >> $OUTPUT
+echo "  <h1>SOC09108 - 2015</h1>" >> $OUTPUT
+
+echo "  <h2>Lectures</h2>" >> $OUTPUT
+echo "  <nav>" >> $OUTPUT
+echo "    <ul>" >> $OUTPUT
 for filepath in `find "./lecture" -maxdepth 1 -mindepth 1 -type d| sort`; do
   name=`basename "$filepath"`
-  echo "    <li><a href=\"$filepath/index.html\" target=\"_blank\">$name</a></li>" >> $OUTPUT
+  echo "      <li><a href=\"$filepath/index.html\" target=\"_blank\">$name</a></li>" >> $OUTPUT
 done
-echo "  </ul>" >> $OUTPUT
+echo "    </ul>" >> $OUTPUT
+echo "  </nav>" >> $OUTPUT
+
+echo "  <h2>Practicals</h2>" >> $OUTPUT
+echo "  <nav>" >> $OUTPUT
+echo "    <ul>" >> $OUTPUT
+for filepath in `find "./practical" -maxdepth 1 -mindepth 1 -type d| sort`; do
+  name=`basename "$filepath"`
+  echo "      <li><a href=\"$filepath/index.html\" target=\"_blank\">$name</a></li>" >> $OUTPUT
+done
+echo "    </ul>" >> $OUTPUT
+echo "  </nav>" >> $OUTPUT
+
 echo "</body>" >> $OUTPUT
 echo "</html>" >> $OUTPUT
